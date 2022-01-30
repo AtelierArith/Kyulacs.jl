e205583d9f99243d5decc9bfbf451f2b433efc48
Documenter.jl <documenter@juliadocs.github.io>
2022-01-30 13:44:04 +0000

    build based on fba6aa8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Kyulacs","category":"page"},{"location":"#Kyulacs","page":"Home","title":"Kyulacs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Kyulacs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Kyulacs]","category":"page"}]
+}
