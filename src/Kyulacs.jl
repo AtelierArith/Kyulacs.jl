5389dde0a6e1de577ebe122700633f68351f68fa
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-01-30 12:52:21 +0900

    update

--- src/Kyulacs.jl
+++ src/Kyulacs.jl
@@ -1,5 +1,35 @@
 module Kyulacs
 
+using PyCall
+
+export qulacs
+
+const qulacs=PyNULL()
+
+for class in [:Observable, :QuantumState, :QuantumCircuit]
+    @eval begin
+        struct $class
+            pyobj::PyObject
+            $(class)(args...)=new(qulacs.$class(args...))
+        end
+
+        function Base.propertynames(t::$(class))
+            propertynames(getfield(t, :pyobj))
+        end
+
+        function Base.getproperty(t::$(class), s::Symbol)
+            if s ∈ fieldnames($(class))
+                return getfield(t, s)
+            else
+                return getproperty(getfield(t, :pyobj), s)
+            end
+        end
+    end
+end
+
 # Write your package code here.
+function __init__()
+    copy!(qulacs, pyimport("qulacs"))
+end
 
 end
