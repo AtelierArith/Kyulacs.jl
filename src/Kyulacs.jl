90dc52065e8f420a1ac67fca81d944419d9ef71a
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2022-10-08 20:35:02 +0900

    pritn_configurations

--- src/Kyulacs.jl
+++ src/Kyulacs.jl
@@ -26,4 +26,20 @@ include("quantum_operator.jl")
 # qulacs.state
 include("state.jl")
 
+function print_configurations()
+	metadata = pyimport("importlib.metadata")
+	println("-- PyCall.jl settings --")
+    @show PyCall.pyversion
+    @show Base.Filesystem.contractuser(PyCall.libpython)
+    @show Base.Filesystem.contractuser(PyCall.pyprogramname)
+    @show PyCall.conda
+    println("-- Python dependencies --")
+	qulacs_version = metadata.distribution("qulacs").version
+	@show qulacs_version
+	scipy_version = metadata.distribution("scipy").version
+	@show scipy_version
+	nothing
+end
+
+
 end # module Kyulacs
